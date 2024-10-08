9545cec803ac14c5ac1273d140d2a0f3ad88a8bd
adan <adanabdullahi343@gmail.com>
2024-10-08 09:06:13 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+newHeader.textContent = 'Adan is the champion';
+
+
